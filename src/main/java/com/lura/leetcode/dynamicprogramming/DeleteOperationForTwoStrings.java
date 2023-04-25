10945411448bac451980deb6b4fd483585796a77
Liu Ran <445999306@qq.com>
2023-04-25 17:23:13 +0800

    1143. 最长公共子序列

---
+++ src/main/java/com/lura/leetcode/dynamicprogramming/DeleteOperationForTwoStrings.java
@@ -0,0 +1,16 @@
+package com.lura.leetcode.dynamicprogramming;
+
+/**
+ * 583. 两个字符串的删除操作
+ * 给定两个单词 word1 和 word2 ，返回使得 word1 和  word2 相同所需的最小步数。
+ * 每步 可以删除任意一个字符串中的一个字符。
+ *
+ * @ description: EleteOperationForTwoStrings
+ * @ author: Liu Ran
+ * @ data: 4/25/23 17:03
+ */
+public class DeleteOperationForTwoStrings {
+    public int minDistance(String word1, String word2) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/lura/leetcode/dynamicprogramming/LongestCommonSubsequenceTest.java
@@ -0,0 +1,20 @@
+package com.lura.leetcode.dynamicprogramming;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @ description: LongestCommonSubsequenceTest
+ * @ author: Liu Ran
+ * @ data: 4/25/23 17:17
+ */
+class LongestCommonSubsequenceTest {
+
+    private final LongestCommonSubsequence solution = new LongestCommonSubsequence();
+    @Test
+    void longestCommonSubsequence() {
+        int actual = solution.longestCommonSubsequence("abcde", "ace");
+        assertEquals(3, actual);
+    }
+}
