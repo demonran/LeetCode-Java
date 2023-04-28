cc6ab9c1638be861a529a36b39831e36a0c8414b
ran.liu1 <445999306@qq.com>
2023-04-28 14:38:21 +0800

    785. 判断二分图

---
+++ src/test/groovy/com/lura/leetcode/graph/IsGraphBipartiteSpec.groovy
@@ -0,0 +1,22 @@
+package com.lura.leetcode.graph
+
+import spock.lang.Specification
+
+/**
+ * @ description: IsGraphBipartiteSpec
+ * @ author: Liu Ran
+ * @ data: 4/28/23 14:19
+ */
+class IsGraphBipartiteSpec extends Specification {
+  var solution = new IsGraphBipartite()
+
+  def "IsBipartite"() {
+    expect:
+    solution.isBipartite(graph) == ok
+    where:
+    graph                                           || ok
+    [[1, 2, 3], [0, 2], [0, 1, 3], [0, 2]] as int[][] || false
+    [[1, 3], [0, 2], [1, 3], [0, 2]] as int[][]       || true
+
+  }
+}
