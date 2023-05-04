358e96aa0227e900e83b0ade049dd76b57220e9b
ran.liu1 <ran.liu1@thoughtworks.com>
2023-05-04 21:27:42 +0800

    82. 删除排序链表中的重复元素 II

---
+++ src/test/groovy/com/lura/leetcode/linkedlist/RemoveDuplicatesFromSortedList2Test.groovy
@@ -0,0 +1,23 @@
+package com.lura.leetcode.linkedlist
+
+import com.lura.leetcode.struct.ListNodeFactory
+import spock.lang.Specification
+
+/**
+ * @ description: RemoveDuplicatesFromSortedList2Test
+ * @ author: Liu Ran
+ * @ data: 5/4/23 21:14
+ */
+class RemoveDuplicatesFromSortedList2Test extends Specification {
+  var solution = new RemoveDuplicatesFromSortedList2()
+
+  def "DeleteDuplicates"() {
+    expect:
+    solution.deleteDuplicates(ListNodeFactory.from(head)) == ListNodeFactory.from(result)
+    where:
+    head                  || result
+    [1, 2, 3, 3, 4, 4, 5] || [1, 2, 5]
+    [1, 1, 1, 2, 3]       || [2, 3]
+    [1, 1]                || []
+  }
+}
