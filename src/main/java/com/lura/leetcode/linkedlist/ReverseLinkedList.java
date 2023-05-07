c58e903ec580b5af37ed0896899186c401ff715a
ran.liu1 <ran.liu1@thoughtworks.com>
2023-05-07 11:10:00 +0800

    206. 反转链表 遍历方法

--- src/main/java/com/lura/leetcode/linkedlist/ReverseLinkedList.java
+++ src/main/java/com/lura/leetcode/linkedlist/ReverseLinkedList.java
@@ -21,4 +21,16 @@ public class ReverseLinkedList {
         return last;
     }
 
+    public ListNode reverseListV2(ListNode head) {
+
+        ListNode pre = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
 }
--- src/test/groovy/com/lura/leetcode/linkedlist/ReverseLinkedListSpec.groovy
+++ src/test/groovy/com/lura/leetcode/linkedlist/ReverseLinkedListSpec.groovy
@@ -1,19 +1,32 @@
 package com.lura.leetcode.linkedlist
 
 import com.lura.leetcode.struct.ListNode
+import com.lura.leetcode.struct.ListNodes
 import spock.lang.Specification
 
 class ReverseLinkedListSpec extends Specification {
 
-    var solution = new ReverseLinkedList()
+  var solution = new ReverseLinkedList()
 
-    def "ReverseList"() {
-        expect:
-        ListNode.toList(solution.reverseList(ListNode.from(head))) == result
-        where:
-        head                     || result
-        [1, 2, 3, 4, 5] as int[] || [5, 4, 3, 2, 1]
-        [1, 2] as int[]          || [2, 1]
-        [] as int[]              || []
-    }
+  def "ReverseList"() {
+    def actual = solution.reverseList(ListNodes.from(head))
+    expect:
+    ListNodes.toList(actual) == result
+    where:
+    head            || result
+    [1, 2, 3, 4, 5] || [5, 4, 3, 2, 1]
+    [1, 2]          || [2, 1]
+    []              || []
+  }
+
+  def "ReverseListV2"() {
+    def actual = solution.reverseListV2(ListNodes.from(head))
+    expect:
+    ListNodes.toList(actual) == result
+    where:
+    head            || result
+    [1, 2, 3, 4, 5] || [5, 4, 3, 2, 1]
+    [1, 2]          || [2, 1]
+    []              || []
+  }
 }
