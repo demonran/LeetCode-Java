1ea5be4092ab0b3efa44efdce99820b3dbdabfc8
ran.liu1 <ran.liu1@thoughtworks.com>
2023-05-13 09:59:58 +0800

    876. 链表的中间结点

---
+++ src/main/java/com/lura/leetcode/linkedlist/MiddleOfTheLinkedList.java
@@ -0,0 +1,25 @@
+package com.lura.leetcode.linkedlist;
+
+import com.lura.leetcode.struct.ListNode;
+
+/**
+ * 876. 链表的中间结点
+ * 给你单链表的头结点 head ，请你找出并返回链表的中间结点。
+ * 如果有两个中间结点，则返回第二个中间结点。
+ *
+ * @ description: MiddleOfTheLinkedList
+ * @ author: Liu Ran
+ * @ data: 5/13/23 09:56
+ */
+public class MiddleOfTheLinkedList {
+
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
