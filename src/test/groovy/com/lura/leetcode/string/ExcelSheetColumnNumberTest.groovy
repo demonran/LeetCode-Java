dd7917124456c456b1a6ac0f118f24ce6c65b627
ran.liu1 <445999306@qq.com>
2023-05-25 13:39:26 +0800

    171. Excel 表列序号

---
+++ src/test/groovy/com/lura/leetcode/string/ExcelSheetColumnNumberTest.groovy
@@ -0,0 +1,22 @@
+package com.lura.leetcode.string
+
+import spock.lang.Specification
+
+/**
+ * @ description: ExcelSheetColumnNumberTest
+ * @ author: Liu Ran
+ * @ data: 5/25/23 13:34
+ */
+class ExcelSheetColumnNumberTest extends Specification {
+  var solution = new ExcelSheetColumnNumber()
+
+  def "TitleToNumber"() {
+    expect:
+    solution.titleToNumber(title) == num
+    where:
+    title || num
+    "A"   || 1
+    "AB"  || 28
+    "ZY"  || 701
+  }
+}
