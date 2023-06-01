6c5d32b713682fe03e79990c7d28d91685184337
ran.liu1 <ran.liu1@thoughtworks.com>
2023-06-01 15:59:56 +0800

    412. Fizz Buzz

---
+++ src/main/java/com/lura/leetcode/string/FizzBuzz.java
@@ -0,0 +1,35 @@
+package com.lura.leetcode.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 412. Fizz Buzz
+ * 给你一个整数 n ，找出从 1 到 n 各个整数的 Fizz Buzz 表示，并用字符串数组 answer（下标从 1 开始）返回结果，其中：
+ * @ description: FizzBuzz
+ * answer[i] == "FizzBuzz" 如果 i 同时是 3 和 5 的倍数。
+ * answer[i] == "Fizz" 如果 i 是 3 的倍数。
+ * answer[i] == "Buzz" 如果 i 是 5 的倍数。
+ * answer[i] == i （以字符串形式）如果上述条件全不满足。
+ *
+ * @ author: Liu Ran
+ * @ data: 6/1/23 15:55
+ */
+public class FizzBuzz {
+
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                ans.add("FizzBuzz");
+            }else if (i % 3 ==0) {
+                ans.add("Fizz");
+            } else if (i % 5 == 0) {
+                ans.add("Buzz");
+            }else {
+                ans.add(String.valueOf(i));
+            }
+        }
+        return ans;
+    }
+}
