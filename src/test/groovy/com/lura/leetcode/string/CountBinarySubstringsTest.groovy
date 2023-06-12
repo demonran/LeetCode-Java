255fb2179eacec9653f54144b5dc489ec8617e90
ran.liu1 <ran.liu1@thoughtworks.com>
2023-06-12 17:21:41 +0800

    696. 计数二进制子串

---
+++ src/test/groovy/com/lura/leetcode/string/CountBinarySubstringsTest.groovy
@@ -0,0 +1,30 @@
+package com.lura.leetcode.string
+
+import spock.lang.Specification
+
+/**
+ * @ description: CountBinarySubstringsTest
+ * @ author: Liu Ran
+ * @ data: 6/12/23 17:12
+ */
+class CountBinarySubstringsTest extends Specification {
+  def solution = new CountBinarySubstrings()
+
+  def "CountBinarySubstrings"() {
+    expect:
+    solution.countBinarySubstrings(s) == res
+    where:
+    s          || res
+    "00110011" || 6
+    "10101"    || 4
+  }
+
+  def "CountBinarySubstringsV2"() {
+    expect:
+    solution.countBinarySubstringsV2(s) == res
+    where:
+    s          || res
+    "00110011" || 6
+    "10101"    || 4
+  }
+}
