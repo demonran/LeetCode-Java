8429586600a5ddac8f55b711d4b485f12f25738d
ran.liu1 <445999306@qq.com>
2023-06-20 17:21:57 +0800

    12. 整数转罗马数字

---
+++ src/test/groovy/com/lura/leetcode/string/IntegerToRomanTest.groovy
@@ -0,0 +1,49 @@
+package com.lura.leetcode.string
+
+import spock.lang.Specification
+
+/**
+ * @ description: IntegerToRomanTest
+ * @ author: Liu Ran
+ * @ data: 6/20/23 16:16
+ */
+class IntegerToRomanTest extends Specification {
+  def solution = new IntegerToRoman()
+
+  def "IntToRoman"() {
+    expect:
+    solution.intToRoman(num) == res
+    where:
+    num  || res
+    3    || "III"
+    4    || "IV"
+    9    || "IX"
+    58   || "LVIII"
+    1994 || "MCMXCIV"
+  }
+
+  def "IntToRomanV2"() {
+    expect:
+    solution.intToRomanV2(num) == res
+    where:
+    num  || res
+    3    || "III"
+    4    || "IV"
+    9    || "IX"
+    58   || "LVIII"
+    1994 || "MCMXCIV"
+  }
+
+  def "IntToRomanV3"() {
+    expect:
+    solution.intToRomanV3(num) == res
+    where:
+    num  || res
+    3    || "III"
+    4    || "IV"
+    9    || "IX"
+    58   || "LVIII"
+    80   || "LXXX"
+    1994 || "MCMXCIV"
+  }
+}
