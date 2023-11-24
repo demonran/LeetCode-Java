0712a2d550efafd1279d513004eec021c901d663
ran.liu <445999306@qq.com>
2023-11-24 22:01:13 +0800

    Top100: 2. 两数相加

---
+++ src/main/java/com/lura/leetcode/top100/AddTwoNumbers.java
@@ -0,0 +1,39 @@
+package com.lura.leetcode.top100;
+
+import com.lura.leetcode.struct.ListNode;
+
+/**
+ * AddTwoNumbers
+ * 2. 两数相加
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ *
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ *
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * @author Liu Ran
+ */
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int a = l1 == null ? 0 : l1.val;
+            int b = l2 == null ? 0 : l2.val;
+            int sum = a + b + carry;
+            // 计算进位
+            carry = sum / 10;
+            // 创建节点
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            // 向后移动
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummy.next;
+    }
+}
