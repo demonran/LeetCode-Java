cf3f0ba9901123c78a2384ee41c23b53dd25d24c
ran.liu <445999306@qq.com>
2023-12-05 21:55:12 +0800

    Top100: 33. 搜索旋转排序数组

---
+++ src/main/java/com/lura/leetcode/top100/SearchInRotatedSortedArray.java
@@ -0,0 +1,34 @@
+package com.lura.leetcode.top100;
+
+/**
+ * SearchInRotatedSortedArray
+ *
+ * @author Liu Ran
+ */
+public class SearchInRotatedSortedArray {
+
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) { // 左边是有序的
+                if (nums[0] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else { // 右边是有序的
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
